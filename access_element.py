31047b127d88b31eadd6b5799da3f195c12499df
ananthanandanan <ananthanandanan@gmail.com>
2020-04-17 01:45:14 +0530

    added the new test code of nawab

--- access_element.py
+++ access_element.py
@@ -1,11 +1,21 @@
 # importing pandas package
 import pandas as pd
+import config 
+import csv
  
 # making data frame from csv file also indexed column will be left most and search related
-data = pd.read_csv("nba.csv")
+data = pd.read_csv("nawab.csv")
 
-ser = pd.Series(data['Name'])
+ser = pd.Series(data['id'])
 
-for i in range(5):
-    print(ser[i])
+for i in ser:
+    print(int(i))
+tweet = 1233423442   
+name = str(tweet) 
 
+    
+with open('nawab.csv', 'a') as f:
+    append=csv.writer(f)
+    append.writerow(name)
+    
+print("success")
