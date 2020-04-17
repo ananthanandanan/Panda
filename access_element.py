5f7436585dbdae3a28f53736768ab8cd7ffebc4e
ananthanandanan <ananthanandanan@gmail.com>
2020-04-17 16:35:08 +0530

    new testing for writing

--- access_element.py
+++ access_element.py
@@ -6,16 +6,17 @@ import csv
 # making data frame from csv file also indexed column will be left most and search related
 data = pd.read_csv("nawab.csv")
 
-ser = pd.Series(data['id'])
+ser = pd.Series(data['Id'])
 
 for i in ser:
     print(int(i))
-tweet = 1233423442   
-name = str(tweet) 
+tweet = 1231223434
+name = ['',tweet]
+
 
     
 with open('nawab.csv', 'a') as f:
     append=csv.writer(f)
     append.writerow(name)
-    
 print("success")
+
