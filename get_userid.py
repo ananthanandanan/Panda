74adf47beb6082ebd82246cefc2defb16d07894d
ananthanandanan <ananthanandanan@gmail.com>
2020-05-13 23:19:07 +0530

    removed keys

--- get_userid.py
+++ get_userid.py
@@ -1,10 +1,10 @@
 import tweepy
 
 
-consumer_key = 'M7Or3e6x1pByV0y4ycGVQX25Q'
-consumer_secret = 'r8nvKpmsq96xKaifGHofk2VIleXPR8QSDa7td5Yp9tDDz6Kq75'
-access_token = '786588110624653313-38yuLzjviUt4i5CGcLfiEOvmVo8zJXD'
-access_token_secret = '8WJGWMl6J8EVHYsyAOQi2IqE8EqogdOqBoHNF2tBt9h6U'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret =  ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
